5d81a91e6b8ed2b6530de72c4813cea4c5b5dd7a
neclor <neclordev@gmail.com>
2025-01-07 00:40:33 +0100

    Update res.gd

--- addons/res/res.gd
+++ addons/res/res.gd
@@ -2,7 +2,7 @@ class_name RES
 extends Object
 
 
-## Functions for finding real roots of polynomial equation of degree up to 4 (quartic).
+## Equation solver for finding real roots of equations up to 4th degree for Godot.
 ##
 ## @tutorial(Wikipedia: Linear equation): https://en.wikipedia.org/wiki/Linear_equation
 ## @tutorial(Wikipedia: Quadratic equation): https://en.wikipedia.org/wiki/Quadratic_equation
